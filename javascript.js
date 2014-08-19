4144e5dff39a5cbe525b8fa327642230868d4c4f
Tumaini Ndossi <tumandossi@gmail.com>
2014-08-19 15:21:40 +0300

    Applying the 'break'

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-javascript: var answer = prompt("You! What is the value of 2 + 2?", ""); if (answer == "4")   alert("You must be a genius or something."); else if (answer == "3" || answer == "5")   alert("Almost!"); else   alert("You're an embarrassment.");
+var answer;
+while (true) {
+  answer = prompt("You! What is the value of 2 + 2?", "");
+  if (answer == "4") {
+    alert("You must be a genius or something.");
+    break;
+  }
+  else if (answer == "3" || answer == "5") {
+    alert("Almost!");
+  }
+  else {
+    alert("You're an embarrassment.");
+  }
+}
