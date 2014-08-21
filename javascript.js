96edde17f2317c04dea4307d6d3afbdceb1aa71e
Tumaini Ndossi <tumandossi@gmail.com>
2014-08-21 10:02:03 +0300

    Functions with side effect

--- javascript.js
+++ javascript.js
@@ -1,14 +1,5 @@
-var answer;
-while (true) {
-  answer = prompt("You! What is the value of 2 + 2?", "");
-  if (answer == "4") {
-    alert("You must be a genius or something.");
-    break;
-  }
-  else if (answer == "3" || answer == "5") {
-    alert("Almost!");
-  }
-  else {
-    alert("You're an embarrassment.");
-  }
+function yell(message) {
+  alert(message + "!!");
 }
+
+yell("Yow");
